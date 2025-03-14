49874b95771dfea4c94d2d5ea4ddcb4690f8fb96
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2025-03-14 19:39:42 +0200

    setup hw07

---
+++ HW_07/main.py
@@ -0,0 +1,31 @@
+# main.py
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from assist_bot import parse_input, add_contact, change_contact, show_phone, show_all
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Sorry, invalid command")
+
+if __name__ == "__main__":
+    main()
